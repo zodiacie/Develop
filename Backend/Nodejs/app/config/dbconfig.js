7ca2e9eaee37058b5f70021275f0ae262d22a712
zodiacie <57634982+zodiacie@users.noreply.github.com>
2023-07-28 14:20:42 -0400

    Delete Backend/Nodejs directory

--- Backend/Nodejs/app/config/dbconfig.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql');
-
-module.exports = mysql.createConnection({
-    host: 'localhost', 
-    user: 'root',
-    password: '123456', 
-    port: '3306',
-    database: 'test'
-}); 
