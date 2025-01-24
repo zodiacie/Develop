5f5f379d33aabaf5e2501221d57376b39c1ed335
zodiacie <57634982+zodiacie@users.noreply.github.com>
2025-01-23 23:37:39 -0500

    Delete Usings.cs

--- Usings.cs
+++
@@ -1,8 +0,0 @@
-global using NUnit.Framework;
-global using System;
-global using System.Diagnostics;
-global using Newtonsoft.Json.Linq;
-global using Newtonsoft.Json;
-global using OpenQA.Selenium;
-global using OpenQA.Selenium.Chrome;
-global using RestSharp;
